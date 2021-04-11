1d9c87180adedb7ce02a7db59e9bfdb94561aba9
ParkJieun <zeun0725@naver.com>
2021-04-11 19:08:50 +0900

    Update perfect-number.py

--- perfect-number.py
+++ perfect-number.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/perfect-number/submissions/
+
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         return True if sum([_num + (num / _num) for _num in range(1, int(sqrt(num)+1)) if num % _num == 0]) - num == num and num > 1 else False
